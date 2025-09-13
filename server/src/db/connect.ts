26582fc757e9c3ebe1523f95724b1941f80e0717
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-09-13 12:54:17 +0300

    fix:build error

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 import { env } from "../config/env";
+
 export const connectDB = async () => {
+  if (!env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in environment variables");
+  }
+
   await mongoose.connect(env.MONGO_URI);
   console.log("MongoDB connected");
 };
